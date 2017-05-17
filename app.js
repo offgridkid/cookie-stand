055c089c7295135a2f492eb6b4f33ca2ed2f48a1
YOUR FULL NAME <justindavenport@Justins-MBP.lan>
2017-05-16 20:18:38 -0700

    added empty random customers method

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
 'use strict';
-//test
-var firstAndPike = {
-  min: 23,
-  max: 65,
-  avgCookies: 6.3,
-  avgCust: function(minCust, maxCust) {
-    return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-  }
-  cph: function(minCust, maxCust) {
-    return this.avgCookies * this.avgCust;
-  }
-};
+
+// var firstAndPike = {
+//   min: 23,
+//   max: 65,
+//   avgCookies: 6.3,
+//   avgCust: function(minCust, maxCust) {
+//     return Math.floor(Math.random() * (this.max - this.min)) + this.min;
+//   }
+// };
 
 
-// var seatacAirport = {
-//   min: 3,
-//   max: 24,
-//   avgCookies: 1.2,
-//   avgCust: function(minCust, maxCust) {
-//     return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-//   }
-// };
+
 //
-// var seattleCenter = {
-//   min: 11,
-//   max: 38,
-//   avgCookies: 3.7,
-//   avgCust: function(minCust, maxCust) {
-//     return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-//   }
-// };
-//
-// var capitolHill = {
-//   min: 20,
-//   max: 38,
-//   avgCookies: 2.3,
-//   avgCust: function(minCust, maxCust) {
-//     return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-//   }
-// };
-//
-// var alki = {
-//   min: 2,
-//   max: 16,
-//   avgCookies: 4.6,
-//   avgCust: function(minCust, maxCust) {
-//     return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-//   }
-// };
+// create constructor definition
+function Store(location, min, max, avg) {
+  this.location = location; // dynamic location property
+  this.min = min; // dynamic min property
+  this.max = max; // dynamic max property
+  this.avg = avg; // dynamic avg property
+}
+
+// add randomCustomers method to the prototype chain
+Store.prototype.randomCustomers = function() {
+
+
+
+}
+// ?i dont get ittttttt
+// ?i need the thing at the top in object literal translated to constructor or something
+// randomCustomers() has been added to data.push loop thing
+
+// add new stores to our table by instantiating new Store objects
+var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
+var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
+var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
+var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
+var alki = new Store('Alki', 2, 16, 4.6);
+
+var stores = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
+
+var table = document.getElementById('shell');
+var data = [];
+
+for (var i = 0; i < stores.length; i++) {
+  data.push(
+    '<td>' + stores[i].location + '</td>' +
+    '<td>' + stores[i].min + '</td>' +
+    '<td>' + stores[i].max + '</td>' +
+    '<td>' + stores[i].avg + '</td>' +
+    '<td>' + stores[i].randomCustomers() + '</td>'
+  );
+}
+
+var new_row;
+
+for (var j = 0; j < data.length; j++) {
+  new_row = document.createElement('tr');
+  new_row.innerHTML = data[j];
+  table.appendChild(new_row);
+}
