15ccf7b584160acba543e200b39ba79e7911d182
YOUR FULL NAME <justindavenport@Justins-MacBook-Pro.local>
2017-05-18 08:53:16 -0700

    further comments

--- app.js
+++ app.js
@@ -1,61 +1,7 @@
 'use strict';
 
-// Yesterday, day 1 of this project, I couldn't figure out how to
-// do pretty much any of the "sales.html"  stuff which set me pretty far behind.
-//  I also don't understand what sales.html even is, as I'm writing javascript
-// in a file named app.js .
-//
-//  So step 2 was to create a random number of customers using our given
-//  minimum and maximum.
-//
-//  When using object literals, I found that number with this,
-//
-// var firstAndPike = {
-//   min: 23,
-//   max: 65,
-//   avgCookies: 6.3,
-//   avgCust: function(minCust, maxCust) {
-//     return Math.floor(Math.random() * (this.max - this.min)) + this.min;
-//   }
-// };
-//
-//  and thats where I stopped.
-//  So,
-//  Now today I have made the table, not list, with all the known/given info, but don't
-// know how to go back and input the stuff from monday
-// 1) random number of customers
-// 2) average cookies per customer * random # of customers
-// 3) total cookies per location
-//
-//
-// I haven't done anything in a .html file either?
-//
-//
-
-
-
-// most comments from here on out were copy and pasted from a codepen example
-//
-// create constructor definition
-// function Store(location, min, max, avg) {
-//   this.location = location; // dynamic location property
-//   this.min = min; // dynamic min property
-//   this.max = max; // dynamic max property
-//   this.avg = avg; // dynamic avg property
-// }
-
-
-// add new stores to our table by instantiating new Store objects
-// var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
-// var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
-// var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
-// var capitolHill = new Store('Capitol Hill', 20, 38, 2.3);
-// var alki = new Store('Alki', 2, 16, 4.6);
 //
 // var stores = [firstAndPike, seaTacAirport, seattleCenter, capitolHill, alki];
-//
-// var table = document.getElementById('shell');
-// var data = [];
 //
 // for (var i = 0; i < stores.length; i++) {
 //   data.push(
@@ -77,24 +23,66 @@
 
 
 //
-
+// this is to make sure our
 var form = document.getElementById('sample_form');
 var table = document.getElementById('store_table');
 
 var data = [];
 
+// create Store constructor
 function Store(location, minCust, maxCust, avg) {
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
-  this.avg = avg;
+  this.avg = avg; //better change this to avgCooPerCus
 }
 
 // add randomCustomers method to the prototype chain
-Store.prototype.randomCustomers = function() {
+Store.prototype.ranCusPerHour = function() {
   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
 };
 
+// add a method that multiplies ranCusPerHour by avgCooPerCus
+Store.prototype.cooSoldPerHour = function() {
+  // i dont know where to start
+  return Store.ranCusPerHour * this.avg;
+};
+
+// Matthews code below
+//
+// Store.prototype.cookiesPerHour = function() {
+//   var cookies_arr = [];
+//   for (var i = 0; i < 15; i++) {
+//     //console.log(this.maxCust, this.minCust);
+//     var custNum = this.minCust + Math.floor(Math.random() * (this.maxCust - this.minCust + 1));
+//     // cookies per hour = avg cookies sold * custNum (need whole number))
+//     var cookiesSold =  Math.floor(custNum * this.avgPerSale);
+//     cookies_arr.push(cookiesSold);
+//   }
+//   return cookies_arr;
+// };
+//
+// I see he has made an empty array then a loop that calculates
+// first a random number of customers between our declared min and max and then
+// second it multiplies those numbers by that stores declared average cookies per
+// customers and pushes the result into our new array.
+//
+// So if I were going to model mine after this it would look like
+// Store.prototype.cooSoldPerHour = function() {
+//   var cookies_arr = [];
+//   for (var i = 0; i < 15; i++) {
+//     var ranCusPerHour = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+//     // cookies per hour = avg cookies sold * custNum (need whole number))
+//     var cooSoldPerHour =  Math.floor(ranCusPerHour * this.avgCooPerCus);
+//     cookies_arr.push(cooSoldPerHour);
+//   }
+//   return cookies_arr;
+// };
+//
+// I want to see how I would use the result of a ranCusPerHour prototype in
+// a second prototype to find cooSoldPerHour, rather than one method to do both.
+
+// add new stores to our table by instantiating new Store objects
 var firstAndPike = new Store('First and Pike', 23, 65, 6.3);
 var seaTacAirport = new Store('SeaTac Airport', 3, 24, 1.2);
 var seattleCenter = new Store('Seattle Center', 11, 38, 3.7);
